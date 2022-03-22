932ed5fa2a7daa93b16d0d1cba18ab63a405079b
Johannes Scribante <johannes.scri@gmail.com>
2022-03-22 06:38:19 +0200

    changed email fields to mail.Address built-in field

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 ## [Unreleased]
+## [1.0.0] - 2022-03-22
+### Added
+- Changed all the email address fields from `string` to `mail.Address` to better
+align with built-in Go features.
 
 ## [Released]
 ## [0.0.0] - 2022-03-21
--- models.go
+++ models.go
@@ -1,8 +1,11 @@
 package emailserv
 
-import "strings"
+import (
+	"net/mail"
+	"strings"
+)
 
-type Headers map[string][]string
+type Headers mail.Header
 
 // Add appends a slice of values to the beginning of the slice of header
 // values.
@@ -10,13 +13,13 @@ func (h *Headers) Add(key string, values []string) {
 	(*h)[key] = append(values, (*h)[key]...)
 }
 
-// Get returns the first value for a given key.
-func (h *Headers) Get(key string) string {
-	if len((*h)[key]) == 0 {
-		return ""
-	}
-	return (*h)[key][0]
-}
+//// Get returns the first value for a given key.
+//func (h *Headers) Get(key string) string {
+//	if len((*h)[key]) == 0 {
+//		return ""
+//	}
+//	return (*h)[key][0]
+//}
 
 // MarshalKey parses all the values for a key into the format needed for
 // the header to be included into an email.
@@ -28,12 +31,12 @@ func (h *Headers) MarshalKey(key string) string {
 }
 
 type Message struct {
-	Headers Headers  `json:"headers"`
-	From    string   `json:"from"`
-	To      []string `json:"to"`
-	CC      []string `json:"cc"`  // TODO: to be added
-	BCC     []string `json:"bcc"` // TODO: to be added
-	ReplyTo string   `json:"replyTo"`
-	Subject string   `json:"subject"`
-	Body    string   `json:"body"`
+	Headers Headers        `json:"headers"`
+	From    mail.Address   `json:"from"`
+	To      []mail.Address `json:"to"`
+	CC      []mail.Address `json:"cc"`
+	BCC     []mail.Address `json:"bcc"`
+	ReplyTo mail.Address   `json:"replyTo"`
+	Subject string         `json:"subject"`
+	Body    string         `json:"body"`
 }
