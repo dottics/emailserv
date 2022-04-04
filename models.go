d38398518a0701dc10e514ab740fff345deaeb3a
Johannes Scribante <johannes.scri@gmail.com>
2022-04-04 17:55:04 +0200

    added message validate method to check minimal requirements

--- models.go
+++ models.go
@@ -30,6 +30,23 @@ func (h *Headers) MarshalKey(key string) string {
 	return strings.Join((*h)[key], "; ")
 }
 
+// Message is the structure that is to be used to create and send an email
+// the Body specifically should be HTML including a meta tag such as
+//
+// <html lang="en">
+// <head>
+// 	<meta charset="UTF-8">
+// 	<style>...add your styling here...</style>
+// </head>
+// <body>
+// 	...add your content here...
+//  ...all images should be included as embedded image or SVG...
+// </body>
+// </html>
+//
+// The API you are building should essentially take the data and use the
+// HTML Template Execute functionality built into go to populate the
+// Message Body before sending.
 type Message struct {
 	Headers Headers        `json:"headers"`
 	From    mail.Address   `json:"from"`
@@ -39,4 +56,28 @@ type Message struct {
 	ReplyTo mail.Address   `json:"replyTo"`
 	Subject string         `json:"subject"`
 	Body    string         `json:"body"`
+}
+
+func (m *Message) Validate() map[string][]string {
+	var errors = make(map[string][]string, 1)
+	if m.From.Address == "" {
+		errors["from"] = []string{"address required"}
+	}
+	if len(m.To) == 0 {
+		errors["to"] = []string{"minimum 1 address"}
+	} else {
+		if m.To[0].Address == "" {
+			errors["to"] = []string{"address required"}
+		}
+	}
+	if m.ReplyTo.Address == "" {
+		errors["replyTo"] = []string{"address required"}
+	}
+	if m.Subject == "" {
+		errors["subject"] = []string{"required"}
+	}
+	if m.Body == "" {
+		errors["body"] = []string{"required"}
+	}
+	return errors
 }
